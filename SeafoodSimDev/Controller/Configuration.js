92ef9164a2bad5f72c15d0e3cc2ef8e485b7fb64
Seregfaroth <bl@tokni.com>
2016-09-07 13:05:28 +0100

    added Configuration class for scenario variables, not done yet

---
+++ SeafoodSimDev/Controller/Configuration.js
@@ -0,0 +1,12 @@
+var Configuration = (function () {
+    function Configuration() {
+    }
+    Configuration.prototype.toJSON = function () {
+        var ret;
+        return ret;
+    };
+    Configuration.prototype.fromJSON = function (p_object) {
+    };
+    return Configuration;
+}());
+//# sourceMappingURL=Configuration.js.map
---
+++ SeafoodSimDev/Controller/Configuration.ts
@@ -0,0 +1,24 @@
+﻿class Configuration {
+    private m_prices;
+    private m_taxingRate;
+    private m_map;
+    private m_shipOwner;
+    private m_shipPrice;
+    private m_aiBuyShipBalance;
+
+    private m_schoolsInOnePlace;
+
+    private financialMaxScore;
+    private environmentalMaxScore;
+    private socialMaxScore;
+
+    constructor() {
+    }
+    toJSON(): Configuration {
+        var ret: Configuration;
+
+        return ret;
+    }
+    fromJSON(p_object) {
+    }
+}
